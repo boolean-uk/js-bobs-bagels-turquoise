045515181b59872f149038cabe089f91533b19d2
dearshrewdwit <dearshrewdwit@gmail.com>
2022-01-18 08:26:49 +0000

    prepare project

---
+++ src/inventory.js
@@ -0,0 +1,56 @@
+
+
+const inventory = [
+    {
+    "sku": "BGLO",
+    "price": 0.49,
+    "name": "Bagel",
+    "variant": "Onion",
+    "discount": "6 for 2.49",
+    "saving": -0.49,
+    "discountTrigger": 6
+    },
+    {
+    "sku": "BGLP",
+    "price": 0.39,
+    "name": "Bagel",
+    "variant": "Plain",
+    "discount": "12 for 3.99",
+    "saving": -0.69
+    },
+    {
+    "sku": "BGLE",
+    "price": 0.49,
+    "name": "Bagel",
+    "variant": "Everything",
+    "discount": "6 for 2.49",
+    "saving": -0.49
+    },
+    {
+    "sku": "BGLS",
+    "price": 0.49,
+    "name": "Bagel",
+    "variant": "Sesame"
+    },
+    {
+    "sku": "COF",
+    "price": 0.99,
+    "name": "Bagel",
+    "variant": ""
+    },
+    {
+    "sku": "BGSE",
+    "price": 2.99,
+    "name": "Bagel Sandwich",
+    "variant": "Everything",
+    },
+    {
+    "sku": "BGSS",
+    "price": 4.99,
+    "name": "Bagel Sandwich",
+    "variant": "Sesame",
+    }   
+      
+];
+
+module.exports = inventory

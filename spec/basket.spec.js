4187c76b2ce87195c3f5b3026137182b2d1fd259
Paolo Federle <federlepaolo95@gmail.com>
2022-11-07 17:43:43 +0100

    correcting test and adding to test all the code

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -10,150 +10,141 @@ describe("Basket", () => {
 
   it("adds 1 item to basket", () => {
     // set up
-     const expected = [{
-        "sku": "BGLO",
-        "price": 0.49,
-        "name": "Bagel",
-        "variant": "Onion",
-        "discount": "6 for 2.49",
-        "saving": -0.49
-        }]
+    const expected = [{
+      "sku": "BGLO",
+      "price": 0.49,
+      "name": "Bagel",
+      "variant": "Onion",
+      "discount": "6 for 2.49",
+      "saving": -0.45,
+      "discountTrigger": 6
+    }]
     // execute
     // basket.this.basketSize = 4
     basket.addToBasket("BGLO")
     const result = basket.basketArray
     // verify
-     expect(result).toEqual(expected);
-   })
+    expect(result).toEqual(expected);
+  })
 
   it("adds 3 items to basket", () => {
-   // set up
+    // set up
     const expected = [{
-        "sku": "BGLO",
-        "price": 0.49,
-        "name": "Bagel",
-        "variant": "Onion",
-        "discount": "6 for 2.49",
-        "saving": -0.49
-        },
-        {
-        "sku": "BGLP",
-        "price": 0.39,
-        "name": "Bagel",
-        "variant": "Plain",
-        "discount": "12 for 3.99",
-        "saving": -0.69
-        },
-        {
-        "sku": "BGLE",
-        "price": 0.49,
-        "name": "Bagel",
-        "variant": "Everything",
-        "discount": "6 for 2.49",
-        "saving": -0.49
-        }]
+      "sku": "BGLO",
+      "price": 0.49,
+      "name": "Bagel",
+      "variant": "Onion",
+      "discount": "6 for 2.49",
+      "saving": -0.45,
+      "discountTrigger": 6
+    },
+    {
+      "sku": "BGLP",
+      "price": 0.39,
+      "name": "Bagel",
+      "variant": "Plain",
+      "discount": "12 for 3.99",
+      "saving": -0.69,
+      "discountTrigger": 12
+    },
+    {
+      "sku": "BGLE",
+      "price": 0.49,
+      "name": "Bagel",
+      "variant": "Everything",
+      "discount": "6 for 2.49",
+      "saving": -0.45,
+      "discountTrigger": 6
+    }]
 
-   // execute
-//    basket.this.basketSize = 4
+    // execute
+    //    basket.this.basketSize = 4
     basket.addToBasket("BGLO")
     basket.addToBasket("BGLP")
     basket.addToBasket("BGLE")
     const result = basket.basketArray
-   // verify
+    // verify
     expect(result).toEqual(expected);
   })
 
   it("remove items from basket", () => {
     // set up
-     const expected = [{
-        "sku": "BGLE",
-    "price": 0.49,
-    "name": "Bagel",
-    "variant": "Everything",
-    "discount": "6 for 2.49",
-    "saving": -0.49
-        }]
+    const expected = [{
+      "sku": "BGLE",
+      "price": 0.49,
+      "name": "Bagel",
+      "variant": "Everything",
+      "discount": "6 for 2.49",
+      "saving": -0.45,
+      "discountTrigger": 6
+    }]
     // execute
-    // basket.this.basketSize = 4
     basket.addToBasket("BGLO")
-    basket.addToBasket("BGLP")
     basket.addToBasket("BGLE")
     basket.removeItems("BGLO")
-    basket.removeItems("BGLP")
-     const result = basket.basketArray
+    const result = basket.basketArray
     // verify
-     expect(result).toEqual(expected);
-   })
+    expect(result).toEqual(expected);
+  })
 
-   it("trys to add more than one item to a basket that is full", () => {
+  it("trys to add more than one item to a basket that is full", () => {
     // set up
     const expected = ("WARNING - Basket is full")
     // execute
-    // basket.this.basketSize = 4
+    basket.basketSize = 1
     basket.addToBasket("BGLO")
-    basket.addToBasket("BGLP")
-    basket.addToBasket("BGLE")
-    basket.addToBasket("COF")
     const result = basket.addToBasket("BGSE")
     // verify
-     expect(result).toEqual(expected);
-   })
+    expect(result).toEqual(expected);
+  })
 
-//    it("trys to add an item that doesn't exist", () => {
-//     // set up
-//     const expected = ("This item does not exist")
-//     // execute
-//     // basket.this.basketSize = 4
-    
-//     const result = basket.addToBasket("BGLL")
-//     // verify
-//      expect(result).toEqual(expected);
-//    })
-
-    it("allows a manager to increase basket size if required", () => {
+  it("allows a manager to increase basket size if required", () => {
     // set up
-    const expected = [ {
-        "sku": "BGLO",
-        "price": 0.49,
-        "name": "Bagel",
-        "variant": "Onion",
-        "discount": "6 for 2.49",
-        "saving": -0.49
-        },
-        {
-        "sku": "BGLP",
-        "price": 0.39,
-        "name": "Bagel",
-        "variant": "Plain",
-        "discount": "12 for 3.99",
-        "saving": -0.69
-        },
-        {
-        "sku": "BGLE",
-        "price": 0.49,
-        "name": "Bagel",
-        "variant": "Everything",
-        "discount": "6 for 2.49",
-        "saving": -0.49
-        },
-        {
-        "sku": "BGLS",
-        "price": 0.49,
-        "name": "Bagel",
-        "variant": "Sesame"
-        },
-        {
-        "sku": "COF",
-        "price": 0.99,
-        "name": "Bagel",
-        "variant": ""
-        },
-        {
-        "sku": "BGSE",
-        "price": 2.99,
-        "name": "Bagel Sandwich",
-        "variant": "Everything",
-        }]
+    const expected = [{
+      "sku": "BGLO",
+      "price": 0.49,
+      "name": "Bagel",
+      "variant": "Onion",
+      "discount": "6 for 2.49",
+      "saving": -0.45,
+      "discountTrigger": 6
+    },
+    {
+      "sku": "BGLP",
+      "price": 0.39,
+      "name": "Bagel",
+      "variant": "Plain",
+      "discount": "12 for 3.99",
+      "saving": -0.69,
+      "discountTrigger": 12
+    },
+    {
+      "sku": "BGLE",
+      "price": 0.49,
+      "name": "Bagel",
+      "variant": "Everything",
+      "discount": "6 for 2.49",
+      "saving": -0.45,
+      "discountTrigger": 6
+    },
+    {
+      "sku": "BGLS",
+      "price": 0.49,
+      "name": "Bagel",
+      "variant": "Sesame"
+    },
+    {
+      "sku": "COF",
+      "price": 0.99,
+      "name": "Bagel",
+      "variant": ""
+    },
+    {
+      "sku": "BGSE",
+      "price": 2.99,
+      "name": "Bagel Sandwich",
+      "variant": "Everything",
+    }]
     // execute
     basket.basketSize = 6
     basket.addToBasket("BGLO")
@@ -164,95 +155,87 @@ describe("Basket", () => {
     basket.addToBasket("BGSE")
     const result = basket.basketArray
     // verify
-        expect(result).toEqual(expected);
-    }) 
+    expect(result).toEqual(expected);
+  })
 
-    it("It can't remove items from basket that doesn't exist", () => {
-        // set up
-         const expected = ("That item isn't in your basket")
-        // execute
-        // basket.this.basketSize = 4
-    
-         const result = basket.removeItems(99)
-        // verify
-         expect(result).toEqual(expected);
-       })
+  it("It can't remove items from basket that doesn't exist", () => {
+    // set up
+    const expected = ("That item isn't in your basket")
+    // execute
+
+    const result = basket.removeItems("BGLP")
+    // verify
+    expect(result).toEqual(expected);
+  })
 
 
-       it("returns the price of an item", () => {
-        // set up
-         const expected = [2.99]
-        // execute
-        // basket.this.basketSize = 4
-        basket.checkPrice("BGSE")
-        const result = basket.priceArray
-        // verify
-         expect(result).toEqual(expected);
-       })
+  it("returns the price of an item", () => {
+    // set up
+    const expected = [2.99]
+    // execute
+    // basket.this.basketSize = 4
+    basket.checkPrice("BGSE")
+    const result = basket.priceArray
+    // verify
+    expect(result).toEqual(expected);
+  })
 
-       it("returns the price of all items in the basket", () => {
-        // set up
-         const expected = 5.84
-        // execute
-        // basket.this.basketSize = 4
-        basket.basketSize = 6
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLP")
-        basket.addToBasket("BGLE")
-        basket.addToBasket("BGLS")
-        basket.addToBasket("COF")
-        basket.addToBasket("BGSE")
-        basket.checkPrice("BGLO")
-        basket.checkPrice("BGLP")
-        basket.checkPrice("BGLE")
-        basket.checkPrice("BGLS")
-        basket.checkPrice("COF")
-        basket.checkPrice("BGSE")
-        const result = basket.totalBasketPrice()
-        // verify
-         expect(result).toEqual(expected);
-       })
+  it("returns the price of all items in the basket", () => {
+    // set up
+    const expected = 5.84
+    // execute
+    // basket.this.basketSize = 4
+    basket.basketSize = 6
+    for (let i = 0; i < 2; i++) {
+      basket.addToBasket("BGLO")
+      basket.addToBasket("BGLP")
+      basket.addToBasket("BGLE")
+      basket.addToBasket("BGLS")
+      basket.addToBasket("COF")
+      basket.addToBasket("BGSE")
+    }
 
-       it("Applies special offer pricing to the basket total", () => {
-        // set up
-         const expected = 2.94
-        // execute
-        basket.basketSize = 50
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLP")
-        basket.addToBasket("BGLE")
-        basket.addToBasket("BGLS")
-        basket.addToBasket("COF")
-        basket.addToBasket("BGSE")
-        basket.addToBasket("BGLP")
-        basket.addToBasket("BGLE")
-        basket.addToBasket("BGLS")
-        basket.addToBasket("COF")
-        basket.addToBasket("BGSE")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLO")
-        basket.addToBasket("BGLP")
-        basket.addToBasket("BGLE")
-        basket.addToBasket("BGLS")
-        basket.addToBasket("COF")
-        basket.addToBasket("BGSE")
-        basket.addToBasket("BGLP")
-        basket.addToBasket("BGLE")
-        basket.addToBasket("BGLS")
-        basket.addToBasket("COF")
-        basket.addToBasket("BGSE")
+    const result = basket.totalBasketPrice()
+    // verify
+    expect(result).toEqual(expected);
+  })
 
-        const result = basket.totalBasketPrice()
-        // verify
-         expect(result).toEqual(expected);
-       })
+  it("Applies special offer pricing to the basket total", () => {
+    // set up
+    const expected = 23.89
+    // execute
+    basket.basketSize = 50
+    for (let i = 0; i < 6; i++) {
+      basket.addToBasket("BGLO")
+    }
+    for (let i = 0; i < 4; i++) {
+      basket.addToBasket("BGLP")
+      basket.addToBasket("BGLE")
+      basket.addToBasket("BGLS")
+      basket.addToBasket("COF")
+      basket.addToBasket("BGSE")
+    }
+
+    const result = basket.totalBasketPrice()
+    // verify
+    expect(result).toEqual(expected);
+  })
+
+  it("Applies special offer pricing once to different product", () => {
+    // set up
+    const expected = 10.34
+    // execute
+    basket.basketSize = 30
+    for (let i = 0; i < 13; i++) {
+      basket.addToBasket("BGLP")
+    }
+    for (let i = 0; i < 7; i++) {
+      basket.addToBasket("BGLO")
+      basket.addToBasket("BGLE")
+    }
+
+    const result = basket.totalBasketPrice()
+    // verify
+    expect(result).toEqual(expected);
+  })
 })
--- src/basket.js
+++ src/basket.js
@@ -10,14 +10,15 @@ class Basket {
         this.count = 0;
     }
     addToBasket(sku) {
-         for(let i = 0; i < menu.length; i++){
-             if (menu[i].sku === sku && this.basketArray.length < this.basketSize){
+        for (let i = 0; i < menu.length; i++) {
+            if (menu[i].sku === sku && this.basketArray.length < this.basketSize) {
                 this.basketArray.push(menu[i])
-             }
-         }
-         return "WARNING - Basket is full"
+                this.checkPrice(sku)
+            }
         }
-    
+        return "WARNING - Basket is full"
+    }
+
     removeItems(sku) {
         for (var i = 0; i < this.basketArray.length; i++) {
             if (this.basketArray[i].sku === sku) {
@@ -25,72 +26,62 @@ class Basket {
             }
         }
         return "That item isn't in your basket"
-        }
-
-    checkPrice(sku){
-        for(let i = 0; i < menu.length; i++){
-        if (menu[i].sku === sku){
-        // console.log("sku", menu[i])
-        this.priceArray.push(menu[i].price)  
     }
-        }
-        }
 
-    totalBasketPrice(){
+    checkPrice(sku) {
+        for (let i = 0; i < menu.length; i++) {
+            if (menu[i].sku === sku) {
+                this.priceArray.push(menu[i].price)
+            }
+        }
+    }
+
+    totalBasketPrice() {
         let totalPrice = 0
         for (let i = 0; i < this.priceArray.length; i++) {
             totalPrice += this.priceArray[i]
         }
-        totalPrice = totalPrice - this.discountedPrice()
-    return Number(totalPrice.toFixed(2))
- }
+        let discountedPrice = this.discountedPrice()
+        totalPrice = totalPrice + discountedPrice
+        return Number(totalPrice.toFixed(2))
+    }
 
     discountedPrice() {
-
-    for (let i = 0; i < this.basketArray.length; i++){
-        this.discountedArray.push(this.basketArray[i].sku)  
+        for (let i = 0; i < this.basketArray.length; i++) {
+            this.discountedArray.push(this.basketArray[i].sku)
         }
-        this.count = this.discountedArray.reduce((tally, sku) => 
-        
-        {tally[sku] = (tally[sku] || 0) + 1;
+        this.count = this.discountedArray.reduce((tally, sku) => {
+            tally[sku] = (tally[sku] || 0) + 1;
             return tally;
-        } , {}) 
+        }, {})
         let totalDiscount = 0;
         const skus = Object.keys(this.count)
         for (let i = 0; i < skus.length; i++) {
             const count = this.count[skus[i]]
             const item = this.getItem(skus[i])
             if (item.discount) {
-                if (count >= item.discountTrigger){
+                if (count >= item.discountTrigger) {
                     totalDiscount += item.saving * (Math.floor(count / item.discountTrigger))
+                    console.log("totaldiscount is ", totalDiscount)
                 }
             }
-    
         }
-        
-    return totalDiscount
-        
-        }
-        
+        return totalDiscount
+    }
+
     getItem(sku) {
-        
-            for(let i = 0; i < menu.length; i++){
-            if (menu[i].sku === sku){
-            return menu[i]  
-        }
+        for (let i = 0; i < menu.length; i++) {
+            if (menu[i].sku === sku) {
+                return menu[i]
             }
- }
-    
-        
-    
+        }
+    }
+
     //this discountedPrice function is now returning an object which contains
     //the name of each SKU and the amount of times it occurs 
-    
+
     // for (let i = 0; i < this.discountedArray.length; i++)
     // if (this.discountedArray[i].sku === ""
-
-
-    
 
 }
 
